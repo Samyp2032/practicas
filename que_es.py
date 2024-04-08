c7a1688a1c132fb5cb314806ddc58b68e15e3490
Samuel Perez <jose.caballero49@uabc.edu.mx>
2024-04-08 09:10:29 -0700

    se añade un archivo me dice que tipo de valor es positivo,negativo o nulo

---
+++ que_es.py
@@ -0,0 +1,11 @@
+num = int(input("Dame un numero: "))
+
+if num > 0:
+    print("El numero es positivo")
+
+else:
+    if num < 0:
+        print("El numero es negativo")
+
+    else: 
+        print("El numero es nulo")
