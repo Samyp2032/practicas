b31fc4e1498d65c72c85a9fd377f97fb2bdf0632
Samuel Perez <jose.caballero49@uabc.edu.mx>
2024-04-08 09:11:02 -0700

    se añade un archivo me dice que tipo de valor es positivo,negativo o nul

---
+++ 3cifras.py
@@ -0,0 +1,15 @@
+num = int(input("Dame un numero: "))
+
+if num <= 9 and num >= 0:
+    print("El numero tiene una cifra")
+
+else: 
+    if num >=10 and num <=99:
+        print("El numero tiene 2 cifras")
+
+    else: 
+        if num >=100 and num <=999:
+            print("El numero tiene 3 cifras")
+        
+        else:
+            print("El numero es de 4 cifras es error")
