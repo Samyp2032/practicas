eb3ba76e36701e8950102695bd293e9c8525d4a1
Samuel Perez <jose.caballero49@uabc.edu.mx>
2024-04-29 08:32:42 -0700

    se añade las evaluaciones

--- evaluacion1.2.py
+++ evaluacion1.2.py
@@ -1,24 +1,23 @@
 import random
 
-def generar_lista_aleatoria(n):
-    lista_aleatoria = [random.randint(1, 100) for _ in range(n)]  # Genera n números aleatorios en el rango de 1 a 100
-    return lista_aleatoria
-
-def crear_diccionario_cuadrados(lista_aleatoria):   #creamos la funcion dicciinario al cuadrado
+def diccionario_cuadrados(lista_aleatoria):   #creamos la funcion dicciinario al cuadrado
     diccionario_cuadrados = {}
-    for i, num in enumerate(lista_aleatoria, start=1):      #Creamos un for que empieza de 1
-        diccionario_cuadrados[i] = num ** 2
+    for i, numeros in enumerate(lista_aleatoria):      #Creamos un for que empieza de 1
+        diccionario_cuadrados[i] = numeros ** 2
     return diccionario_cuadrados    #Regresa el diccionario
 
-def main():
-    n = int(input("Ingrese la cantidad de números aleatorios a generar: "))     #Pedimos el tamaño de n
+def lista_aleatoria(n):
+    lista_aleatoria = []
+    for _ in range(n):
+        lista_aleatoria.append(random.randint(1, 100))
+    return lista_aleatoria
 
-    lista_aleatoria = generar_lista_aleatoria(n)
-    diccionario_cuadrados = crear_diccionario_cuadrados(lista_aleatoria)
-    
-    print("Lista de números aleatorios:", lista_aleatoria)
-    print("Diccionario de cuadrados:")
-    for key, value in diccionario_cuadrados.items():    #Hacemos el valor de la llava con el cuadrado
-        print(f"{key}: {value}")
+n = int(input("Cantidad de que se van a generar: "))     #Pedimos el tamaño de n
 
-main()
+lista_aleatoria = lista_aleatoria(n)
+diccionario_cuadrados = diccionario_cuadrados(lista_aleatoria)
+
+print("Lista de números aleatorios:", lista_aleatoria)
+print("Diccionario de cuadrados:")
+for llave, valor in diccionario_cuadrados.items():    #Hacemos el valor de la llava con el cuadrado
+    print(f"{llave}: {valor}")
