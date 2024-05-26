0fcd02b35ac4cd622c71fdb2c671cf138e50467f
Samyp2032 <117222644+Samyp2032@users.noreply.github.com>
2024-05-25 21:05:51 -0700

    Agregamos la practica 8

--- sns5_0.py
+++
@@ -1,9 +0,0 @@
-#%%
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-from PIL import Image
-img = Image.open('chart.jpeg')
-img_array = np.array(img)
-plt.imshow(img);
-# %%
